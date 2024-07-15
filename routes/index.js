2ec273384c087f0519e2357fa46605d69c0fac47
Suman Das <sum4ndas@gmail.com>
2024-07-15 09:15:18 +0530

    Import and use post, comment and user REST API routes

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,30 @@ const express = require("express");
 const router = express.Router();
 const asyncHandler = require("express-async-handler");
 const User = require("../models/user");
+const Post = require("../models/post");
+const Comment = require("../models/comment");
+
+// Import routers.
+const postRouter = require("./post");
+const commentRouter = require("./comment");
+const userRouter = require("./user");
+
+// Use imported routers.
+router.use("/", postRouter);
+router.use("/", commentRouter);
+router.use("/", userRouter);
 
 // GET index page
 router.get(
   "/",
   asyncHandler(async (req, res, next) => {
-    const allUsers = await User.find({}).exec();
+    const [allUsers, allPosts, allComments] = await Promise.all([
+      User.find({}).exec(),
+      Post.find({}).populate("comments").exec(),
+      Comment.find({}).populate("author").exec(),
+    ]);
 
-    res.json({ users: allUsers });
+    res.json({ users: allUsers, posts: allPosts, comments: allComments });
   })
 );
 
