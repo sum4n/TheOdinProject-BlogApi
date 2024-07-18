de052f29db9413326f0c5247bdc15fd497a21fcd
Suman Das <sum4ndas@gmail.com>
2024-07-18 07:14:24 +0530

    Add 'timestamps' property

--- models/user.js
+++ models/user.js
@@ -2,26 +2,30 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  first_name: { type: String, required: true, maxLength: 30 },
-  last_name: { type: String, required: true, maxLength: 30 },
-  username: { type: String, required: true, maxLength: 30 },
-  email: {
-    type: String,
-    required: true,
-    maxLength: 120,
-    unique: true,
-    lowercase: true,
+const UserSchema = new Schema(
+  {
+    first_name: { type: String, required: true, maxLength: 30 },
+    last_name: { type: String, required: true, maxLength: 30 },
+    username: { type: String, required: true, maxLength: 30 },
+    email: {
+      type: String,
+      required: true,
+      maxLength: 120,
+      unique: true,
+      lowercase: true,
+    },
+    password: { type: String, required: true, maxLength: 120 },
+    user_type: {
+      type: String,
+      required: true,
+      enum: ["admin", "regular"],
+      default: "regular",
+    },
   },
-  password: { type: String, required: true, maxLength: 120 },
-  user_type: {
-    type: String,
-    required: true,
-    enum: ["admin", "regular"],
-    default: "regular",
-  },
-  joined_at: { type: Date, required: true, default: Date.now },
-});
+  {
+    timestamps: true,
+  }
+);
 
 // Virtual for User's URL
 UserSchema.virtual("url").get(function () {
